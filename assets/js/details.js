67be1db13416ef6535a1cb99ba767d3408f25336
RB <ramon.blissenbach.08@gmail.com>
2023-10-28 11:40:44 +0200

    Fixed details Site

--- assets/js/details.js
+++ assets/js/details.js
@@ -25,16 +25,48 @@ searchButton.addEventListener('click', function () {
 });
 
 function updateTable() {
-    getWeatherDate("27.10.2023", function (weather) {
-        var data = JSON.parse(weather);
+    if (url.searchParams.has('date')) {
+        var dateValue = url.searchParams.get('date');
 
-        document.getElementById('temperature').innerText = data.temperature.toFixed(2) + "°C";
-        document.getElementById('pressure').innerText = data.pressure.toFixed(2) + "hPa";
-        document.getElementById('humidity').innerText = data.humidity.toFixed(2) + "%";
-        document.getElementById('precipitation').innerText = data.precipitation.toFixed(2) + "mm";
-        document.getElementById('lux').innerText = data.lux.toFixed(2) + "lx";
-        document.getElementById('uv').innerText = data.uv.toFixed(2) + "µW/cm²";
-    });
+        getWeatherDate(formatDate(dateValue), function (weather) {
+            var data = JSON.parse(weather);
+
+            document.getElementById('temperature').innerText = data.temperature.toFixed(2) + "°C";
+            document.getElementById('pressure').innerText = data.pressure.toFixed(2) + "hPa";
+            document.getElementById('humidity').innerText = data.humidity.toFixed(2) + "%";
+            document.getElementById('precipitation').innerText = data.precipitation.toFixed(2) + "mm";
+            document.getElementById('lux').innerText = data.lux.toFixed(2) + "lx";
+            document.getElementById('uv').innerText = data.uv.toFixed(2) + "µW/cm²";
+        });
+    }
+}
+
+function formatDate(inputDate) {
+    // Create a Date object from the input date string
+    var date = new Date(inputDate);
+
+    // Check if the input date is valid
+    if (isNaN(date)) {
+        return "Invalid Date";
+    }
+
+    // Extract the day, month, and year components
+    var day = date.getDate();
+    var month = date.getMonth() + 1; // Month is zero-based
+    var year = date.getFullYear();
+
+    // Ensure the day and month are formatted as two digits
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+
+    // Create the formatted date string in "DD.MM.yyyy" format
+    var formattedDate = day + "." + month + "." + year;
+
+    return formattedDate;
 }
 
 updateTable();
